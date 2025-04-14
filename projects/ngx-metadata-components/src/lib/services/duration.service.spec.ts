2acc1b7b2fdbd22cbe88c05369fb7a6e41957a1a
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-04-14 09:43:41 +0200

    Add duration service tests

--- projects/ngx-metadata-components/src/lib/services/duration.service.spec.ts
+++ projects/ngx-metadata-components/src/lib/services/duration.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-
 import { DurationService } from './duration.service';
 
 describe('DurationService', () => {
-  let service: DurationService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DurationService);
+  it('should convert seconds to minutes and seconds - 0 seconds', () => {
+    const result = DurationService.convertSecondsToMinutes(0);
+    expect(result).toEqual({ minutes: '00', seconds: '00' });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should convert seconds to minutes and seconds - less than a minute', () => {
+    const result = DurationService.convertSecondsToMinutes(45);
+    expect(result).toEqual({ minutes: '00', seconds: '45' });
+  });
+
+  it('should convert seconds to minutes and seconds - exactly one minute', () => {
+    const result = DurationService.convertSecondsToMinutes(60);
+    expect(result).toEqual({ minutes: '01', seconds: '00' });
+  });
+
+  it('should convert seconds to minutes and seconds - more than a minute', () => {
+    const result = DurationService.convertSecondsToMinutes(125);
+    expect(result).toEqual({ minutes: '02', seconds: '05' });
   });
 });
